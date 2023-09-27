5645dc5196edd2f98c8ec769f79a24f170256cdc
Becky1703 <oluwabukunmia@gmail.com>
2023-09-27 14:03:57 -0700

    pascals triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+"""Function to Implement Pascal's Triangle """
+
+
+def pascal_triangle(n):
+    """Prints a pascal triangle"""
+    if n <= 0:
+        return []
+
+    # Creates an empty triangle
+    triangle = [[1]]
+
+    # Fills the triangle with rows
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
