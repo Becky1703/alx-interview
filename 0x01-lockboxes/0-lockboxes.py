c57b307ce1cd5b2be1fcd7b6d2741d3e9a256a46
Becky1703 <oluwabukunmia@gmail.com>
2023-10-04 03:28:45 -0700

    error fixed

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,26 +4,13 @@
 
 def canUnlockAll(boxes):
     """Method to check if boxes can be unlocked"""
-
-    n = len(boxes)
-
-    # creates a list to track what box has been visited
-    visited = set([0])
-
-    # The first box is unlocked
-    not_visited = set(boxes[0]).difference(set([0]))
-
-    while len(not_visited) > 0:
-        box_idx = not_visited.pop()
-
-        # Check what key is in the current box
-        if not box_idx or box_idx >= n or box_idx < 0:
+    unlocked = [0]
+    for box_id, box in enumerate(boxes):
+        if not box:
             continue
-        # if the key can open a box that hasn't been visited,
-        # mark it as visited
-        if box_idx not in visited:
-            not_visited = not_visited.union(boxes[box_idx])
-            visited.add(box_idx)
-
-        # check if all boxes have been visited
-        return n == len(visited)
+        for key in box:
+            if key < len(boxes) and key not in unlocked and key != box_id:
+                unlocked.append(key)
+    if len(unlocked) == len(boxes):
+        return True
+    return False
